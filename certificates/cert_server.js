10236d75bb9d6fea3a82610d24137026e83e387a
Ace Fox <afox09@gmail.com>
2020-11-18 11:49:36 -0600

    trying again to remove junk from certserver

--- certificates/cert_server.js
+++ certificates/cert_server.js
@@ -1,16 +1,15 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-​
+
 const webrootPath = process.env.CERTBOT_WEBROOT_PATH;
 const port = process.env.CERTSERVER_PORT || 80;
-​
+
 console.log(`Listening on port :80. PID: ${process.pid}`);
-​
+
 //Create a certificate renewal server 
 const certServer = http.createServer((req, res) => {
     console.log('Cert request:', req.url);
-​
     function resolveCertCode(req, res) {
         let urlParts = url.parse(req.url);
         let path = urlParts.pathname;
@@ -21,15 +20,12 @@ const certServer = http.createServer((req, res) => {
             return;
         }
     }
-​
     //Get the cert challenge code if there is one - reject everything else
     let certCode = resolveCertCode(req, res);
     if (certCode) {
-
         //Look for a file created by certbot in the configured path
         let challengePath = `${webrootPath}/.well-known/acme-challenge/${certCode}`
         console.log(`Certificate challenge: ${challengePath}`);
-
         if (fs.existsSync(challengePath)) {
 
             //Read the renewal data file
@@ -42,11 +38,9 @@ const certServer = http.createServer((req, res) => {
             res.end();
             return;
         }
-        console.log('Certificate data NOT found');
+        console.log('Certificate data NOT found'); 
     }
-
     console.log('Cert request rejected');
-
     res.statusCode = 405;
     res.statusMessage = "NotSupported";
     res.write("");
